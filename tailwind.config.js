7bd155048e387b69ed59516d89e04dc74e8afef6
Lukáš Píšek <lukin.pisek@email.cz>
2024-10-11 17:46:18 +0200

    feat: tailwind configuring

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+		colors: {
+			Pet_Header_BG: '#5994CE',
+			Pet_Text: '#B957CE',
+			Pet_UI_Icon: '#B9E9E9',
+			Pet_BG: '#2A2356'
+		}
+	},
+	  fontFamily: {
+		'Pet_Title': ['Press Start 2P', 'sans-serif'],
+	  }
   },
-  plugins: [require("daisyui")],
+  plugins: [
+	  require('daisyui'),
+	  require('react-custom-scrollbars-2')
+  ],
 }
 
