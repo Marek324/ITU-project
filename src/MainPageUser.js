1866dbc0ebff649549866e0d87de4ef7eb13ce7d
Tobiáš Adamčík <tobias.adamcik@outlook.com>
2024-10-14 19:38:33 +0200

    feat: started work on main page

---
+++ src/MainPageUser.js
@@ -0,0 +1,23 @@
+﻿function MainPageUser()
+{
+	let x = "penis"
+	  return (
+	<div className="MainPageUser">
+	  <header className="MainPageUser-header">
+		<Rectangle />
+		<p>
+			{x}
+		</p>
+	  </header>
+	</div>
+  );
+}
+
+function Rectangle() {
+	return (
+		<div className="w-full h-40 bg-Main_Header flex items-center justify-center border-2 border-Main_Header_Border">
+			<h1 className="text-3xl font-Pet_Title text-border">Adopt & Play</h1>
+		</div>
+	);
+}
+export default MainPageUser;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,14 @@ module.exports = {
 			Pet_Header_BG: '#5994CE',
 			Pet_Text: '#B957CE',
 			Pet_UI_Icon: '#B9E9E9',
-			Pet_BG: '#2A2356'
+			Pet_BG: '#2A2356',
+			Main_Header: '#7183C0',
+			Main_Header_Border: '#963FB4'
 		}
 	},
 	  fontFamily: {
-		Pet_Title: ['Press Start 2P', 'sans-serif'],
+		Pet_Title: ['"Press Start 2P"', 'sans-serif'],
+  		Header_Text: ['"Press Start 2P"', 'sans-serif'],
 	  }
   },
   plugins: [
