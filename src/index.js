28f61e86d7a58625adc5a6183e654cdec9284264
Spustnik <1psimonova1@gmail.com>
2024-10-14 18:04:11 +0200

    pardon ja som tam dala nejaku ??mrdku??

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './tailwind.config.js';
+// import './tailwind.config.js';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
