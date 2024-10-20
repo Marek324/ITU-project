28010bc4a6e40380aa90cc11a224eb9f78e2831a
Lukáš Píšek <lukin.pisek@email.cz>
2024-10-20 19:24:43 +0200

    feat: added jump event

--- src/Pet/FlappyBird/Game.js
+++ src/Pet/FlappyBird/Game.js
@@ -1,13 +1,37 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import DownBar from '../components/DownBar.js';
 import TopBar from '../components/TopBar.js';
 import {homeB} from "../../svg";
 
 function Game() {
+	//Handle scroll
+
+	//Event na skákání
+	const jump = () =>
+	{
+		alert("jump");
+	};
+
+	//Reagování na stisk klávesy
+	useEffect(() => {
+		const handleKeyDown = (event) => {
+			if (event.code === 'Space' || event.code === 'ArrowUp') {
+				jump();
+			}
+		};
+
+		window.addEventListener('keydown', handleKeyDown);
+
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
+		};
+	}, []);
+
 	return (
 		<div
 			className="game-container min-h-screen flex flex-col bg-cover bg-center"
 			style={{ backgroundImage: `url('https://i.postimg.cc/JznxpgQY/itukoza.jpg')` }}
+			onClick={jump}
 		>
 			<TopBar
 				title="Flappy Pet"
