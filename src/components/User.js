25b020feb80e1345dc33ad715136e7eedc7a9a90
Marek324 <hrmajo1@gmail.com>
2024-11-08 10:07:12 +0100

    ( :

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import AdoptHeader from "./AdoptHeader";
 
 function User() {
   return (
     <div>
+	  <AdoptHeader />
       <h2>User Page</h2>
       <p>This is the user page content.</p>
     </div>
