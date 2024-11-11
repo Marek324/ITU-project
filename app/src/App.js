654f49800878d79b4f4d1e4ddaf334a1d9884065
Marek324 <hrmajo1@gmail.com>
2024-11-11 22:37:57 +0100

    Updated router

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import MainPageController from "./controllers/MainPageController";
- let userMode = true;
+import 'App.css';
+
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
+import MainPageController from "controllers/MainPageController";
+import AnimalDetails from "./components/AnimalDetails";
+import Game from "./Pet/FlappyBird/Game";
+
 function App() {
-	if(userMode)
-	{
-		return (
-			< MainPageController />
-		);
-	}
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<MainPageController />} />
+				<Route path="/animal/:id" element={<AnimalDetails />} />
+				<Route path="/animal/:id/flappypet" element={<Game />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
