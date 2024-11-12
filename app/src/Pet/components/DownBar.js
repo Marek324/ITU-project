31c0750748b64f11ec66ee68dfd29397535eb34e
Tobiáš Adamčík <tobias.adamcik@outlook.com>
2024-11-12 20:46:30 +0100

    idk petra uprav si kod na tu sipku smd

--- app/src/Pet/components/DownBar.js
+++ app/src/Pet/components/DownBar.js
@@ -1,10 +1,17 @@
 import React, { forwardRef } from 'react';
-import { Return } from '../../svg';
+import {arrow_back as home_icon, Return} from '../../svg';
+import {Link, useLocation} from "react-router-dom";
 
 const DownBar = forwardRef(({ onFirstClick, onSecondClick, onThirdClick, firstIcon, secondIcon, thirdIcon }, ref) => {
+	const location = useLocation();
+	// Extract the animalid from the URL
+	const pathParts = location.pathname.split('/');
+	const animalid = pathParts[2];
+	let link_back = `/animal/${animalid}`;
+
 	return (
 		<div ref={ref} className="bg-[#5994CE] h-16 w-full flex items-center justify-between text-white border-t-2 border-[#B957CE] absolute bottom-0">
-			{Return()}
+			<Link to={link_back} className="col-start-1 content-center">{Return()}</Link>
 			<div className="flex flex-row justify-center items-center space-x-36 flex-1 mr-16">
 				<div onClick={onFirstClick} className="cursor-pointer">
 					{firstIcon}
