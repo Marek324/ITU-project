379a8b655bbcafa5b2207c64b5879cdd5de5d6e0
Tobiáš Adamčík <tobias.adamcik@outlook.com>
2024-12-14 10:47:41 +0100

    refactors, favoriting, updated design, ...

---
+++ app/src/components/animals/AnimalList.js
@@ -0,0 +1,21 @@
+﻿import AnimalsListItem from "./AnimalsListItem";
+import { deleteCross } from "svg";
+
+function RectangleList({ animals, adminMode, handleRemoveAnimal }) {
+	return (
+		<div className="flex flex-col items-center justify-center m-2">
+			{animals.map((animal, index) => (
+				<div key={index} className="flex items-center justify-center w-full">
+					{adminMode && (
+						<button onClick={() => handleRemoveAnimal(animal.id)} className="mr-2">
+							{deleteCross()}
+						</button>
+					)}
+					<AnimalsListItem animal={animal} adminMode={adminMode} />
+				</div>
+			))}
+		</div>
+	);
+}
+
+export default RectangleList;
